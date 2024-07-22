3faf5f43a3f1cef662048e8fa73b7611df9e3da6
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-07-22 15:21:58 -0600

    Got node spinning and printing out

--- mavsim_bridge/mavsim_bridge.py
+++ mavsim_bridge/mavsim_bridge.py
@@ -1,5 +1,20 @@
+import sys
+sys.path.append('..')
+
+import rclpy
+from rclpy.node import Node
+
+class MavSimBridge(Node):
+    def __init__(self):
+        super().__init__('mavsim_bridge')
+        self.get_logger().info('Hi from mavsim_bridge.')
+
+
 def main():
-    print('Hi from mavsim_bridge.')
+    rclpy.init()
+    mavsim_bridge = MavSimBridge()
+    rclpy.spin(mavsim_bridge)
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
